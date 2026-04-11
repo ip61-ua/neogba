aaad854d9b2ecdeb07cb62c67a0c8ad613d8f33f
ip61-ua <ip61@alu.ua.es>
2026-04-11 23:56:35 +0200

    feat: use indexed regs and no pointers

--- include/arm7/cpu.hpp
+++ include/arm7/cpu.hpp
@@ -8,35 +8,97 @@ typedef uint16_t u16;
 typedef uint8_t u8;
 
 struct Registers {
-  u32 r0, r1, r2, r3, r4, r5, r6, r7, pc, cpsr;
-  u32 r8, r9, r10, r11, r12;
-  u32 r13, r14;
-  u32 r8_fiq, r9_fiq, r10_fiq, r11_fiq, r12_fiq;
-  u32 r13_fiq, r14_fiq, spsr_fiq;
-  u32 r13_svc, r14_svc, spsr_svc;
-  u32 r13_abt, r14_abt, spsr_abt;
-  u32 r13_irq, r14_irq, spsr_irq;
-  u32 r13_und, r14_und, spsr_und;
+public:
+  enum RegisterIndex : u8 {
+    r0,
+    r1,
+    r2,
+    r3,
+    r4,
+    r5,
+    r6,
+    r7,
+    pc,
+    cpsr,
+    r8,
+    r9,
+    r10,
+    r11,
+    r12,
+    r13,
+    r14,
+    r8_fiq,
+    r9_fiq,
+    r10_fiq,
+    r11_fiq,
+    r12_fiq,
+    r13_fiq,
+    r14_fiq,
+    spsr_fiq,
+    r13_svc,
+    r14_svc,
+    spsr_svc,
+    r13_abt,
+    r14_abt,
+    spsr_abt,
+    r13_irq,
+    r14_irq,
+    spsr_irq,
+    r13_und,
+    r14_und,
+    spsr_und,
+  };
 
-  u32* user[17] = {&r0, &r1,  &r2,  &r3,  &r4,  &r5,  &r6, &r7,  &r8,
-                   &r9, &r10, &r11, &r12, &r13, &r14, &pc, &cpsr};
+  static constexpr u8 mapUser[17] = {r0, r1,  r2,  r3,  r4,  r5,  r6, r7,  r8,
+                                     r9, r10, r11, r12, r13, r14, pc, cpsr},
+                      mapFiq[18] = {r0,      r1,      r2,      r3,     r4,      r5,
+                                    r6,      r7,      r8_fiq,  r9_fiq, r10_fiq, r11_fiq,
+                                    r12_fiq, r13_fiq, r14_fiq, pc,     cpsr,    spsr_fiq},
+                      mapSvc[18] = {r0, r1,  r2,  r3,  r4,      r5,      r6, r7,   r8,
+                                    r9, r10, r11, r12, r13_svc, r14_svc, pc, cpsr, spsr_svc},
+                      mapAbt[18] = {r0, r1,  r2,  r3,  r4,      r5,      r6, r7,   r8,
+                                    r9, r10, r11, r12, r13_abt, r14_abt, pc, cpsr, spsr_abt},
+                      mapIrq[18] = {r0, r1,  r2,  r3,  r4,      r5,      r6, r7,   r8,
+                                    r9, r10, r11, r12, r13_irq, r14_irq, pc, cpsr, spsr_irq},
+                      mapUnd[18] = {r0, r1,  r2,  r3,  r4,      r5,      r6, r7,   r8,
+                                    r9, r10, r11, r12, r13_und, r14_und, pc, cpsr, spsr_und};
 
-  u32* fiq[18] = {&r0,     &r1,      &r2,      &r3,      &r4,      &r5,      &r6, &r7,   &r8_fiq,
-                  &r9_fiq, &r10_fiq, &r11_fiq, &r12_fiq, &r13_fiq, &r14_fiq, &pc, &cpsr, &spsr_fiq};
+private:
+  u32 regs[37];
+  const u8* mapCurrent = mapUser;
 
-  u32* svc[18] = {&r0, &r1,  &r2,  &r3,  &r4,      &r5,      &r6, &r7,   &r8,
-                  &r9, &r10, &r11, &r12, &r13_svc, &r14_svc, &pc, &cpsr, &spsr_svc};
+public:
+  u32 read(int reg) const {
+    return regs[mapCurrent[reg]];
+  }
+  void write(int reg, u32 val) {
+    regs[mapCurrent[reg]] = val;
+  }
+  u32 read(enum RegisterIndex idx) const {
+    return regs[idx];
+  }
+  void write(enum RegisterIndex idx, u32 val) {
+    regs[idx] = val;
+  }
 
-  u32* abt[18] = {&r0, &r1,  &r2,  &r3,  &r4,      &r5,      &r6, &r7,   &r8,
-                  &r9, &r10, &r11, &r12, &r13_abt, &r14_abt, &pc, &cpsr, &spsr_abt};
-
-  u32* irq[18] = {&r0, &r1,  &r2,  &r3,  &r4,      &r5,      &r6, &r7,   &r8,
-                  &r9, &r10, &r11, &r12, &r13_irq, &r14_irq, &pc, &cpsr, &spsr_irq};
-
-  u32* und[18] = {&r0, &r1,  &r2,  &r3,  &r4,      &r5,      &r6, &r7,   &r8,
-                  &r9, &r10, &r11, &r12, &r13_und, &r14_und, &pc, &cpsr, &spsr_und};
-
-  u32** current = user;
+  void setUser() {
+    mapCurrent = mapUser;
+  }
+  void setFiq() {
+    mapCurrent = mapFiq;
+  }
+  void setAbt() {
+    mapCurrent = mapAbt;
+  }
+  void setIrq() {
+    mapCurrent = mapIrq;
+  }
+  void setUnd() {
+    mapCurrent = mapUnd;
+  }
+  void setSvc() {
+    mapCurrent = mapSvc;
+  }
 };
 
 class CPU {
