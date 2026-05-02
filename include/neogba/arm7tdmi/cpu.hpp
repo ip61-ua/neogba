dc028eca1394c4df4f50e11eafe8790318c14991
ip61-ua <ip61@alu.ua.es>
2026-05-02 23:58:56 +0200

    refactor: update code to match patterns

--- include/neogba/arm7tdmi/cpu.hpp
+++ include/neogba/arm7tdmi/cpu.hpp
@@ -27,25 +27,6 @@ enum Cond : u8 {
   NV = 0b1111, // 1111 = NV - reserved.
 };
 
-enum DataProcessingOpcode : u8 {
-  AND = 0b0000, // AND Logical AND Rd := Rn AND shifter_operand
-  EOR = 0b0001, // EOR Logical Exclusive OR Rd := Rn EOR shifter_operand
-  SUB = 0b0010, // SUB Subtract Rd := Rn - shifter_operand
-  RSB = 0b0011, // RSB Reverse Subtract Rd := shifter_operand - Rn
-  ADD = 0b0100, // ADD Add Rd := Rn + shifter_operand
-  ADC = 0b0101, // ADC Add with Carry Rd := Rn + shifter_operand + Carry Flag
-  SBC = 0b0110, // SBC Subtract with Carry Rd := Rn - shifter_operand - NOT(Carry Flag)
-  RSC = 0b0111, // RSC Reverse Subtract with Carry Rd := shifter_operand - Rn - NOT(Carry Flag)
-  TST = 0b1000, // TST Test Update flags after Rn AND shifter_operand
-  TEQ = 0b1001, // TEQ Test Equivalence Update flags after Rn EOR shifter_operand
-  CMP = 0b1010, // CMP Compare Update flags after Rn - shifter_operand
-  CMN = 0b1011, // CMN Compare Negated Update flags after Rn + shifter_operand
-  ORR = 0b1100, // ORR Logical (inclusive) OR Rd := Rn OR shifter_operand
-  MOV = 0b1101, // MOV Move Rd := shifter_operand (no first operand)
-  BIC = 0b1110, // BIC Bit Clear Rd := Rn AND NOT(shifter_operand)
-  MVN = 0b1111, // MVN Move Not Rd := NOT shifter_operand (no first operand)
-};
-
 namespace ArmInstruction {
 
 struct MultiplyAccumulate {
@@ -231,7 +212,26 @@ struct SignedDataTrans {
 };
 
 struct DataProcessingPSRTrans {
-  u8 opcode;
+  enum opcodeT : u8 {
+    AND = 0b0000, // AND Logical AND Rd := Rn AND shifter_operand
+    EOR = 0b0001, // EOR Logical Exclusive OR Rd := Rn EOR shifter_operand
+    SUB = 0b0010, // SUB Subtract Rd := Rn - shifter_operand
+    RSB = 0b0011, // RSB Reverse Subtract Rd := shifter_operand - Rn
+    ADD = 0b0100, // ADD Add Rd := Rn + shifter_operand
+    ADC = 0b0101, // ADC Add with Carry Rd := Rn + shifter_operand + Carry Flag
+    SBC = 0b0110, // SBC Subtract with Carry Rd := Rn - shifter_operand - NOT(Carry Flag)
+    RSC = 0b0111, // RSC Reverse Subtract with Carry Rd := shifter_operand - Rn - NOT(Carry Flag)
+    TST = 0b1000, // TST Test Update flags after Rn AND shifter_operand
+    TEQ = 0b1001, // TEQ Test Equivalence Update flags after Rn EOR shifter_operand
+    CMP = 0b1010, // CMP Compare Update flags after Rn - shifter_operand
+    CMN = 0b1011, // CMN Compare Negated Update flags after Rn + shifter_operand
+    ORR = 0b1100, // ORR Logical (inclusive) OR Rd := Rn OR shifter_operand
+    MOV = 0b1101, // MOV Move Rd := shifter_operand (no first operand)
+    BIC = 0b1110, // BIC Bit Clear Rd := Rn AND NOT(shifter_operand)
+    MVN = 0b1111, // MVN Move Not Rd := NOT shifter_operand (no first operand)
+  };
+
+  opcodeT opcode;
   bool s;
   u8 rn;
   u8 rd;
@@ -243,7 +243,7 @@ struct DataProcessingPSRTrans {
 
   ARM_INSTRUCTION_EXTRACT(instruction, DataProcessingPSRTrans) {
     return {
-        .opcode = EXTRACT_BIT_MASK(instruction, u8, 21, 0xf),
+        .opcode = EXTRACT_BIT_MASK(instruction, opcodeT, 21, 0xf),
         .s = BIT_TO_BOOL(instruction, 20),
         .rn = EXTRACT_BIT_MASK(instruction, u8, 16, 0xf),
         .rd = EXTRACT_BIT_MASK(instruction, u8, 12, 0xf),
--- src/arm7tdmi/cpu.cpp
+++ src/arm7tdmi/cpu.cpp
@@ -8,32 +8,31 @@ u32 CPU::fetch() {
 }
 
 void CPU::execute() {
-  if (isBranchIns(currentInstruction)) {
-    if (isBranchAndLinkIns(currentInstruction))
+  using namespace ArmInstruction;
+
+  if (Branch::is(currentInstruction)) {
+    Branch info = Branch::extract(currentInstruction);
+
+    if (info.l)
       registers.write(14, registers.read(RegistersIndex::pc) - 4);
 
-    registers.write(RegistersIndex::pc,
-                    ((currentInstruction & 0x00FFFFFF) << 2) + registers.read(RegistersIndex::pc));
+    registers.write(RegistersIndex::pc, (info.offset << 2) + registers.read(RegistersIndex::pc));
     return;
   }
 
-  if (isDataProcessingIns(currentInstruction)) {
-    bool i = currentInstruction & 0x02000000;
-    bool s = currentInstruction & 0x100000;
-    u32 opcode = (currentInstruction & 0x01E00000) >> 24;
-    u32 Rn = currentInstruction & 0xF0000 >> 16;
-    u32 Rd = currentInstruction & 0xF000 >> 12;
+  if (DataProcessingPSRTrans::is(currentInstruction)) {
+    DataProcessingPSRTrans info = DataProcessingPSRTrans::extract(currentInstruction);
 
     /*
-      <opcode1>{<cond>}{S} <Rd>, <shifter_operand>
-      <opcode1> := MOV | MVN
-      <opcode2>{<cond>} <Rn>, <shifter_operand>
-      <opcode2> := CMP | CMN | TST | TEQ
-      <opcode3>{<cond>}{S} <Rd>, <Rn>, <shifter_operand>
-      <opcode3> := ADD | SUB | RSB | ADC | SBC | RSC | AND | BIC | EOR | ORR
-    */
+    <opcode1>{<cond>}{S} <Rd>, <shifter_operand>
+    <opcode1> := MOV | MVN
+    <opcode2>{<cond>} <Rn>, <shifter_operand>
+    <opcode2> := CMP | CMN | TST | TEQ
+    <opcode3>{<cond>}{S} <Rd>, <Rn>, <shifter_operand>
+    <opcode3> := ADD | SUB | RSB | ADC | SBC | RSC | AND | BIC | EOR | ORR
+  */
 
-    switch (opcode) {
+    switch (info.opcode) {
     case DataProcessingOpcode::MOV:
     case DataProcessingOpcode::MVN:
       break;
