bdf5da5cb2be8de14607b1daaa88c8f57df774e2
ip61-ua <ip61@alu.ua.es>
2026-05-14 07:49:09 +0200

    feat: keep transition to c

--- include/neogba/constants.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace neogba {
-
-const auto K_BLOCK_BIT_SIZE{32}, K_BLOCK_INDEX_MASK{8},
-    K_GBABLOCKOFFSETMASK{K_BLOCK_BIT_SIZE - K_BLOCK_INDEX_MASK};
-
-} // namespace neogba
