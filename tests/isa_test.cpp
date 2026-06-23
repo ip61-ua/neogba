65f484800548f975e6b81606ec41acb08bf07f9e
ip61-ua <ip61@alu.ua.es>
2026-06-23 10:44:06 +0200

    fix: test

--- tests/isa_test.cpp
+++ tests/isa_test.cpp
@@ -1,16 +1,16 @@
 #include "neogba/arm7tdmi/isa.hpp"
 #include <gtest/gtest.h>
 
-TEST(IsaTest, ShouldExtractRightBits) {
+TEST(IsaTest, ShouldExtractRightBitsFromRaw) {
   constexpr u32 ins = 0xa13cdbd3;
-  ASSERT_EQ(arm_get_cond(ins), 0xa);
-  ASSERT_EQ(arm_half_data_trans_imm_is_p(ins), true);
-  ASSERT_EQ(arm_half_data_trans_imm_is_u(ins), false);
-  ASSERT_EQ(arm_half_data_trans_imm_is_w(ins), true);
-  ASSERT_EQ(arm_half_data_trans_imm_is_l(ins), true);
-  ASSERT_EQ(arm_half_data_trans_imm_get_rn(ins), 0xc);
-  ASSERT_EQ(arm_half_data_trans_imm_get_rd(ins), 0xd);
-  ASSERT_EQ(arm_half_data_trans_imm_is_s(ins), true);
-  ASSERT_EQ(arm_half_data_trans_imm_is_h(ins), false);
-  ASSERT_EQ(arm_half_data_trans_imm_get_offset(ins), 0xb3);
+  ASSERT_EQ(isa_get_ARM_COND(ins), 0xa);
+  ASSERT_EQ(isa_is_ARM_HALFIMM_P(ins), true);
+  ASSERT_EQ(isa_is_ARM_HALFIMM_U(ins), false);
+  ASSERT_EQ(isa_is_ARM_HALFIMM_W(ins), true);
+  ASSERT_EQ(isa_is_ARM_HALFIMM_L(ins), true);
+  ASSERT_EQ(isa_get_ARM_HALFIMM_RN(ins), 0xc);
+  ASSERT_EQ(isa_get_ARM_HALFIMM_RD(ins), 0xd);
+  ASSERT_EQ(isa_is_ARM_HALFIMM_S(ins), true);
+  ASSERT_EQ(isa_is_ARM_HALFIMM_H(ins), false);
+  ASSERT_EQ(isa_get_ARM_HALFIMM_OFFSET(ins), 0xb3);
 }
