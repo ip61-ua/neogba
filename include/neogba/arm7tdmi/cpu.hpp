c4a5787e0fd18531712a2899d4a6a4eb0125a35a
ip61-ua <ip61@alu.ua.es>
2026-06-28 22:03:33 +0200

    refactor: back to macros on this and make more inlines

--- include/neogba/arm7tdmi/cpu.hpp
+++ include/neogba/arm7tdmi/cpu.hpp
@@ -3,6 +3,20 @@
 
 namespace neogba {
 
+#define ARM7TDMI_REGISTERS_USR                                                                     \
+  r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, pc, cpsr, spsr
+#define ARM7TDMI_REGISTERS_FIQ                                                                     \
+  r0, r1, r2, r3, r4, r5, r6, r7, r8_fiq, r9_fiq, r10_fiq, r11_fiq, r12_fiq, r13_fiq, r14_fiq, pc, \
+      cpsr, spsr_fiq
+#define ARM7TDMI_REGISTERS_IRQ                                                                     \
+  r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_irq, r14_irq, pc, cpsr, spsr_irq
+#define ARM7TDMI_REGISTERS_SVC                                                                     \
+  r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_svc, r14_svc, pc, cpsr, spsr_svc
+#define ARM7TDMI_REGISTERS_ABT                                                                     \
+  r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_abt, r14_abt, pc, cpsr, spsr_abt
+#define ARM7TDMI_REGISTERS_UND                                                                     \
+  r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_und, r14_und, pc, cpsr, spsr_und
+
 struct arm7tdmi {
   enum cpsr_flag : u32 {
     /// @brief Negative (N)
@@ -34,7 +48,7 @@ struct arm7tdmi {
     FIQ = /*      */ 0x0000001c
   };
 
-  enum class cpsr_mode : u8 {
+  enum cpsr_mode : u8 {
     USR = 0b10000,
     FIQ = 0b10001,
     IRQ = 0b10010,
@@ -67,37 +81,35 @@ struct arm7tdmi {
 
   static constexpr u8 N_ACTIVE_REGISTERS = 18, N_REGISTERS = 38;
   static constexpr u8 REGISTERS_PRESET[6][N_ACTIVE_REGISTERS] = {
-      {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, pc, cpsr, spsr},
-      {r0, r1, r2, r3, r4, r5, r6, r7, r8_fiq, r9_fiq, r10_fiq, r11_fiq, r12_fiq, r13_fiq, r14_fiq,
-       pc, cpsr, spsr_fiq},
-      {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_irq, r14_irq, pc, cpsr, spsr_irq},
-      {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_svc, r14_svc, pc, cpsr, spsr_svc},
-      {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_abt, r14_abt, pc, cpsr, spsr_abt},
-      {r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13_und, r14_und, pc, cpsr,
-       spsr_und}};
+      {ARM7TDMI_REGISTERS_USR}, {ARM7TDMI_REGISTERS_FIQ}, {ARM7TDMI_REGISTERS_IRQ},
+      {ARM7TDMI_REGISTERS_SVC}, {ARM7TDMI_REGISTERS_ABT}, {ARM7TDMI_REGISTERS_UND}};
 
   u32 registers[N_REGISTERS];
   u8 active_registers[N_ACTIVE_REGISTERS];
 
-  [[nodiscard]] u32 read_active_register(u8 reg) const;
-  void write_active_register(u8 reg, u32 content);
+  [[nodiscard]] inline u32 read_active_register(u8 reg) const {
+    return this->registers[this->active_registers[reg]];
+  }
+  inline void write_active_register(u8 reg, u32 content) {
+    this->registers[this->active_registers[reg]] = content;
+  }
 
-  [[nodiscard]] inline u32 read_pc() const { return this->registers[pc]; }
-  inline void write_pc(u32 new_pc) { this->registers[pc] = new_pc; }
+  [[nodiscard]] inline u32 read_pc() const { return registers[pc]; }
+  inline void write_pc(u32 new_pc) { registers[pc] = new_pc; }
 
-  [[nodiscard]] inline u32 read_cpsr() const { return this->registers[cpsr]; }
-  inline void write_cpsr(u32 new_cpsr) { this->registers[cpsr] = new_cpsr; }
+  [[nodiscard]] inline u32 read_cpsr() const { return registers[cpsr]; }
+  inline void write_cpsr(u32 new_cpsr) { registers[cpsr] = new_cpsr; }
 
-  [[nodiscard]] bool is_cpsr_bits(u32 mask, u32 bits) const;
-  void clear_cpsr_bits(u32 mask);
-  void set_cpsr_bits(u32 mask, u32 bits);
+  [[nodiscard]] inline bool is_cpsr(u32 mask, u32 bits) const {
+    return (registers[cpsr] & mask) == bits;
+  }
+  inline void clear_cpsr(u32 mask) { registers[cpsr] &= ~mask; }
+  void set_cpsr(u32 mask, u32 bits);
 
-  [[nodiscard]] bool is_mode(cpsr_mode mode) const;
+  [[nodiscard]] inline bool is_mode(cpsr_mode mode) const { return (registers[cpsr] & M) == mode; }
   void set_mode(cpsr_mode mode, bool update_cpsr = true);
 
   static u8 get_idx_registers_preset_by_mode(cpsr_mode mode);
-  static constexpr u8* get_registers_preset(registers_preset_idx mode);
-
   [[nodiscard]] bool ckeck_arm_condition(u32 inst) const;
 
   void empty_registers();
