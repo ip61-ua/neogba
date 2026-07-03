7eaf665f8d2a11db0a4ad3c84d2e82478f93a4cf
ip61-ua <ip61@alu.ua.es>
2026-07-03 14:37:29 +0200

    feat: initial and impl

---
+++ src/arm7tdmi/arm_isa.cpp
@@ -0,0 +1,98 @@
+#include "neogba/arm7tdmi/arm_isa.hpp"
+#include "neogba/arm7tdmi/isa.hpp"
+
+using namespace neogba;
+
+inline void arm_AND(arm7tdmi& cpu, u32 inst) {
+  auto rn = ISA_ARM_FSR_RN::get(inst);
+  auto rd = ISA_ARM_FSR_RD::get(inst);
+  auto i = ISA_ARM_FSR_I::get(inst);
+
+  u32 operable_operand2{};
+
+  if (i) {
+
+    // operand2 is immediate value with shift.
+
+    u32 shift_amount = 2 * ISA_ARM_FSR_OPERAND2_ROTATE::get(inst);
+    u32 imm = ISA_ARM_FSR_OPERAND2_IMM::get(inst);
+
+    operable_operand2 =
+        (shift_amount == 0) ? imm : ((imm >> shift_amount) | (imm << (32 - shift_amount)));
+  } else {
+
+    // operand2 is a register with shift.
+
+    auto rm = cpu.read_active_register(ISA_ARM_FSR_OPERAND2_RM::get(inst));
+    auto shift_type = ISA_ARM_FSR_OPERAND2_SHIFT_TYPE::get(inst);
+    auto four = ISA_ARM_FSR_OPERAND2_4::get(inst);
+
+    u32 shift_amount = four ? cpu.read_active_register(ISA_ARM_FSR_OPERAND2_RS::get(inst))
+                            : ISA_ARM_FSR_OPERAND2_SHIFT_AMOU::get(inst);
+
+    bool is_special_case = !four && shift_amount == 0;
+
+    switch (shift_type) {
+
+    case LSL:
+      if (is_special_case) {
+        operable_operand2 = rm; // LSL #0
+      } else {
+        operable_operand2 = (shift_amount >= 32) ? 0 : (rm << shift_amount);
+      }
+      break;
+
+    case LSR:
+      if (is_special_case)
+        shift_amount = 32; // LSR #0
+
+      operable_operand2 = (shift_amount >= 32) ? 0 : (rm >> shift_amount);
+      break;
+
+    case ASR:
+      if (is_special_case)
+        shift_amount = 32; // ASR #0
+
+      if (shift_amount >= 32) {
+        // Shifting ge 32, a. if and negative -> ffff, b. if and postive -> 0s
+        operable_operand2 = (rm & 0x80000000) ? 0xffffffff : 0;
+      } else {
+        // Preserve sign bit with i32
+        operable_operand2 = static_cast<u32>(static_cast<int32_t>(rm) >> shift_amount);
+      }
+      break;
+
+    case ROR:
+      if (is_special_case) {
+        // RRX: Rotate 1 bit and include Cin.
+        if (cpu.is_cpsr(arm7tdmi::C, arm7tdmi::C))
+          operable_operand2 = 0x80000000;
+
+        operable_operand2 |= (rm >> 1);
+      } else {
+        shift_amount %= 32; // 32 = 64 -> rotate 0 bits
+        operable_operand2 =
+            (shift_amount == 0) ? rm : ((rm >> shift_amount) | (rm << (32 - shift_amount)));
+      }
+      break;
+    }
+  }
+
+  cpu.write_active_register(rd, cpu.read_active_register(rn) & operable_operand2);
+}
+
+inline void arm_EOR(arm7tdmi& cpu, u32 inst);
+inline void arm_SUB(arm7tdmi& cpu, u32 inst);
+inline void arm_RSB(arm7tdmi& cpu, u32 inst);
+inline void arm_ADD(arm7tdmi& cpu, u32 inst);
+inline void arm_ADC(arm7tdmi& cpu, u32 inst);
+inline void arm_SBC(arm7tdmi& cpu, u32 inst);
+inline void arm_RSC(arm7tdmi& cpu, u32 inst);
+inline void arm_TST(arm7tdmi& cpu, u32 inst);
+inline void arm_TEQ(arm7tdmi& cpu, u32 inst);
+inline void arm_CMP(arm7tdmi& cpu, u32 inst);
+inline void arm_CMN(arm7tdmi& cpu, u32 inst);
+inline void arm_ORR(arm7tdmi& cpu, u32 inst);
+inline void arm_MOV(arm7tdmi& cpu, u32 inst);
+inline void arm_BIC(arm7tdmi& cpu, u32 inst);
+inline void arm_MVN(arm7tdmi& cpu, u32 inst);
