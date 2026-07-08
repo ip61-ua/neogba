442fdb87e42168c4122fdb1146112985f81774f5
ip61-ua <ip61@alu.ua.es>
2026-07-08 13:25:39 +0200

    feat: impl lut!

--- include/neogba/structs/lut.hpp
+++ include/neogba/structs/lut.hpp
@@ -1,25 +1,61 @@
 #include "neogba/types.hpp"
 #include <array>
+#include <cstddef>
 
-template <std::size_t max_lenght, typename store_t = void*, typename idx_t = neogba::u8,
+namespace neogba {
+
+template <std::size_t max_length, typename store_t = void*, typename idx_t = u8,
           typename return_t = void>
 class lut {
-  std::array<store_t, max_lenght> storage;
+private:
+  std::array<store_t, max_length> storage{};
+  constexpr void put(std::size_t raw_idx, store_t what) { storage[raw_idx] = what; }
+  constexpr std::size_t fill_recursive(std::size_t base, std::size_t mask, store_t what, bool high,
+                                       std::size_t bit = 0) {
+    // caso base
+    std::size_t shifted{mask >> bit};
+    if (shifted == 0)
+      return 0;
+
+    // caso recursivo no fork
+    bool is_mask_one{(shifted & 0x1u) == 1};
+    if (!is_mask_one)
+      return fill_recursive(base, mask, what, high, ++bit);
+
+    // caso recursivo branch
+    std::size_t bit_mask{1u << bit};
+    bool bit_set{(base & bit_mask) != 0};
+    u8 count{static_cast<u8>(high != bit_set)};
+    base ^= (static_cast<std::size_t>(count) << bit);
+
+    ++bit;
+
+    return count + fill_recursive(base, mask, what, true, bit) +
+           fill_recursive(base, mask, what, false, bit);
+  }
 
 public:
-  virtual std::size_t norm_idx(idx_t idx) const;
+  constexpr virtual std::size_t norm_idx(idx_t idx) const { return static_cast<std::size_t>(idx); }
 
-  store_t get(idx_t idx) const;
-  return_t run(idx_t idx, ...) const;
+  constexpr std::array<store_t, max_length> get_wrapper() const { return storage; }
+  constexpr store_t get(idx_t idx) const { return storage[norm_idx(idx)]; }
+  constexpr return_t run(idx_t idx, auto... params) const { return get(idx)(params...); }
 
-  void put(idx_t idx, store_t what);
-  void fill(idx_t idx_base, store_t what, idx_t mask);
+  constexpr void fill(idx_t idx, store_t what) { storage[norm_idx(idx)] = what; }
+  constexpr std::size_t fill(idx_t idx_base, store_t what, idx_t mask) {
+    std::size_t b{norm_idx(idx_base)}, m{norm_idx(mask)};
+    put(b, what);
+    return 1 + fill_recursive(b, m, what, true) + fill_recursive(b, m, what, false);
+  }
 
-  void erase(idx_t idx);
-
-  bool is_empty() const;
-  bool is_empty(idx_t idx) const;
-
-  std::size_t length() const;
-  std::size_t count_stored(store_t what) const;
+  constexpr std::size_t length() const { return max_length; };
+  constexpr std::size_t count_stored(store_t what) const {
+    std::size_t n{};
+    for (auto elem : storage)
+      if (elem == what)
+        n++;
+    return n;
+  }
 };
+
+} // namespace neogba
--- notes/index.md
+++ notes/index.md
@@ -392,5 +392,68 @@ public:
 
 ```
 
+- Me he dado cuenta que un plantilla no debería ir en un archivo separado
+- lo he juntado todo.
+- he hecho todo a tiempo de compilación.
 
 
+``` c++
+template <std::size_t max_length, typename store_t = void*, typename idx_t = u8,
+          typename return_t = void>
+class lut {
+private:
+  std::array<store_t, max_length> storage{};
+  constexpr void put(std::size_t raw_idx, store_t what) { storage[raw_idx] = what; }
+  constexpr std::size_t fill_recursive(std::size_t base, std::size_t mask, store_t what, bool high,
+                                       std::size_t bit = 0) {
+    // caso base
+    std::size_t shifted{mask >> bit};
+    if (shifted == 0)
+      return 0;
+
+    // caso recursivo no fork
+    bool is_mask_one{(shifted & 0x1u) == 1};
+    if (!is_mask_one)
+      return fill_recursive(base, mask, what, high, ++bit);
+
+    // caso recursivo branch
+    std::size_t exclusive_mask_bit{1u << bit}, exclusive_base_bit{base & (exclusive_mask_bit)};
+    bool is_base_zero{exclusive_base_bit == 0}, is_base_one{not is_base_zero}, low{not high};
+    u8 count{0};
+
+    if ((high and is_base_zero) or (low and is_base_one)) {
+      count = 1;
+      base ^= exclusive_base_bit;
+    }
+
+    ++bit;
+
+    return count + fill_recursive(base, mask, what, true, bit) +
+           fill_recursive(base, mask, what, false, bit);
+  }
+
+public:
+  constexpr virtual std::size_t norm_idx(idx_t idx) const { return static_cast<std::size_t>(idx); }
+
+  constexpr store_t get(idx_t idx) const { return storage[norm_idx(idx)]; }
+  constexpr return_t run(idx_t idx, auto... params) const { return get(idx)(params...); }
+
+  constexpr void fill(idx_t idx, store_t what) { storage[norm_idx(idx)] = what; }
+  constexpr std::size_t fill(idx_t idx_base, store_t what, idx_t mask) {
+    std::size_t b{norm_idx(idx_base)}, m{norm_idx(mask)};
+
+    return fill_recursive(b, m, what, true) + fill_recursive(b, m, what, false);
+  }
+
+  constexpr std::size_t length() const { return max_length; };
+  constexpr std::size_t count_stored(store_t what) const {
+    std::size_t n{};
+    for (auto elem : storage)
+      if (elem == what)
+        n++;
+    return n;
+  }
+};
+```
+
+  - Luego me puse a refactorizar el código para una versión branchless el caso de recursivo branch 
