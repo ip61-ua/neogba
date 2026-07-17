97a544d619d1aab8c01e5364e45e1edb82b323a1
ip61-ua <ip61@alu.ua.es>
2026-07-17 20:06:25 +0200

    chore: delete "duplicated" code

--- include/neogba/arm7tdmi/arm_isa.hpp
+++ include/neogba/arm7tdmi/arm_isa.hpp
@@ -127,24 +127,7 @@ arm_operand2_result arm_operand2_generator(arm7tdmi& cpu, u32 inst) {
 }
 
 inline constexpr auto arm_operand2_lut = []() consteval {
-  constexpr auto
-
-      arm_operand2_compute_i0_40_sa0_LSL{arm_operand2_generator<false, false, false, true, LSL>},
-      arm_operand2_compute_i0_40_sa0_LSR{arm_operand2_generator<false, false, false, true, LSR>},
-      arm_operand2_compute_i0_40_sa0_ASR{arm_operand2_generator<false, false, false, true, ASR>},
-      arm_operand2_compute_i0_40_sa0_ROR{arm_operand2_generator<false, false, false, true, ROR>},
-      arm_operand2_compute_i0_40_sant0_LSL{arm_operand2_generator<false, false, false, false, LSL>},
-      arm_operand2_compute_i0_40_sant0_LSR{arm_operand2_generator<false, false, false, false, LSR>},
-      arm_operand2_compute_i0_40_sant0_ASR{arm_operand2_generator<false, false, false, false, ASR>},
-      arm_operand2_compute_i0_40_sant0_ROR{arm_operand2_generator<false, false, false, false, ROR>},
-      arm_operand2_compute_i0_41_LSL{arm_operand2_generator<false, false, true, false, LSL>},
-      arm_operand2_compute_i0_41_LSR{arm_operand2_generator<false, false, true, false, LSR>},
-      arm_operand2_compute_i0_41_ASR{arm_operand2_generator<false, false, true, false, ASR>},
-      arm_operand2_compute_i0_41_ROR{arm_operand2_generator<false, false, true, false, ROR>},
-      arm_operand2_compute_i1_rotate0{arm_operand2_generator<true, true>},
-      arm_operand2_compute_i1_rotatenot0{arm_operand2_generator<true, false>};
-
-  lut<arm_operand2_result (*)(arm7tdmi&, u32), 1 << 4, arm_operand2_result,
+  lut<arm_operand2_result (*)(arm7tdmi&, u32), 14, arm_operand2_result,
       +[](std::size_t idx) -> std::size_t {
         bool i = ISA_ARM_FSR_I::get_raw(idx);
         bool b4 = ISA_ARM_FSR_OPERAND2_4::get_raw(idx);
@@ -162,21 +145,20 @@ inline constexpr auto arm_operand2_lut = []() consteval {
       }>
       table;
 
-  table.put_raw(0b0000, arm_operand2_compute_i0_40_sa0_LSL);
-  table.put_raw(0b0001, arm_operand2_compute_i0_40_sa0_LSR);
-  table.put_raw(0b0010, arm_operand2_compute_i0_40_sa0_ASR);
-  table.put_raw(0b0011, arm_operand2_compute_i0_40_sa0_ROR);
-  table.put_raw(0b0100, arm_operand2_compute_i0_40_sant0_LSL);
-  table.put_raw(0b0101, arm_operand2_compute_i0_40_sant0_LSR);
-  table.put_raw(0b0110, arm_operand2_compute_i0_40_sant0_ASR);
-  table.put_raw(0b0111, arm_operand2_compute_i0_40_sant0_ROR);
-  table.put_raw(0b1000, arm_operand2_compute_i0_41_LSL);
-  table.put_raw(0b1001, arm_operand2_compute_i0_41_LSR);
-  table.put_raw(0b1010, arm_operand2_compute_i0_41_ASR);
-  table.put_raw(0b1011, arm_operand2_compute_i0_41_ROR);
-  table.put_raw(0b1100, arm_operand2_compute_i1_rotate0);
-  table.put_raw(0b1101, arm_operand2_compute_i1_rotatenot0);
-  table.fill_missing(arm_operand2_compute_i1_rotatenot0);
+  table.put_raw(0b0000, arm_operand2_generator<false, false, false, true, LSL>);
+  table.put_raw(0b0001, arm_operand2_generator<false, false, false, true, LSR>);
+  table.put_raw(0b0010, arm_operand2_generator<false, false, false, true, ASR>);
+  table.put_raw(0b0011, arm_operand2_generator<false, false, false, true, ROR>);
+  table.put_raw(0b0100, arm_operand2_generator<false, false, false, false, LSL>);
+  table.put_raw(0b0101, arm_operand2_generator<false, false, false, false, LSR>);
+  table.put_raw(0b0110, arm_operand2_generator<false, false, false, false, ASR>);
+  table.put_raw(0b0111, arm_operand2_generator<false, false, false, false, ROR>);
+  table.put_raw(0b1000, arm_operand2_generator<false, false, true, false, LSL>);
+  table.put_raw(0b1001, arm_operand2_generator<false, false, true, false, LSR>);
+  table.put_raw(0b1010, arm_operand2_generator<false, false, true, false, ASR>);
+  table.put_raw(0b1011, arm_operand2_generator<false, false, true, false, ROR>);
+  table.put_raw(0b1100, arm_operand2_generator<true, true>);
+  table.put_raw(0b1101, arm_operand2_generator<true, false>);
 
   return table;
 }();
