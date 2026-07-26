04a79a1d2692d2fc0e1d420919c49b6fe4a94afd
ip61-ua <ip61@alu.ua.es>
2026-07-26 17:26:52 +0200

    feat: finish initial bus impl

--- include/neogba/memory/bus.hpp
+++ include/neogba/memory/bus.hpp
@@ -7,15 +7,27 @@ class memory_bus {
   lut<imemory*, 1 << 8, [](std::size_t addr) -> auto { return addr >> 24; }> ms;
 
 public:
-  inline u32 read(u8 size, u32 addr) const { return ms.get(addr)->read(size, addr); }
-  inline bool write(u8 size, u32 addr, u32 contents) {
-    return ms.get(addr)->write(size, addr, contents);
-  }
   inline bool is_null(u32 addr) const { return ms.get(addr) == 0; }
-  inline void attach(u32 addr, imemory* new_memory) { ms.fill(addr, new_memory); }
+  inline u32 read(u8 size, u32 addr) const {
+    return is_null(addr) ? 0 : ms.get(addr)->read(size, addr);
+  }
+  inline bool write(u8 size, u32 addr, u32 contents) {
+    return is_null(addr) ? false : ms.get(addr)->write(size, addr, contents);
+  }
+  inline void attach(u32 addr, imemory* new_memory) {
+    if (!new_memory)
+      return;
+
+    ms.fill(addr, new_memory);
+    new_memory->attach(this);
+  }
   inline void deattach(u32 addr) {
-    ms.get(addr)->deattach();
-    ms.
+    auto mem{ms.get(addr)};
+    if (!mem)
+      return;
+
+    mem->deattach();
+    std::ranges::replace(ms, mem, nullptr);
   }
 };
 
