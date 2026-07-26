f1a6fbc7da95fb2acd5176826b0d3a8e413e0a5f
ip61-ua <ip61@alu.ua.es>
2026-07-26 18:37:23 +0200

    test: basic test for memory bus

--- tests/memory_test.cpp
+++ tests/memory_test.cpp
@@ -4,28 +4,30 @@
 using namespace neogba;
 
 namespace {
+
+// direcciones de 32.
+// tenemos 8 bits más altos -> índice.
+// tenemos 32 - 8 bits más bajos -> despl (24).
+// esta mem ocupa 2^16 bytes
+struct ram_stub : public memory<1 << 15> {};
+
 class bus_test : public ::testing::Test {
 protected:
   std::unique_ptr<memory_bus> bus;
+  std::unique_ptr<ram_stub> ram;
 
   bus_test() {}
 
   virtual ~bus_test() {}
 
-  void SetUp() override { bus = std::make_unique<memory_bus>(); }
+  void SetUp() override {
+    bus = std::make_unique<memory_bus>();
+    ram = std::make_unique<ram_stub>();
+  }
 
   void TearDown() override {}
 };
 
-// direcciones de 32.
-// tenemos 8 bits más altos -> índice.
-// tenemos 32 - 8 bits más bajos -> despl (24).
-
-struct ram_stub : public memory<1 << 15> {};
-
 } // namespace
 
-TEST_F(bus_test, get_idx_registers_preset_by_mode_should_say_right_when_providing_modes) {
-
-  ASSERT_EQ(1, 1);
-}
+TEST_F(bus_test, read_empty) { ASSERT_EQ(1, 1); }
