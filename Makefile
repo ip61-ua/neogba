6e1f41c2a0a2d7b53f617e49ecfc6d582a45ec3f
ip61-ua <ip61@alu.ua.es>
2026-08-12 17:27:42 +0200

    test: add test-case to test newer field compact usage syntax!

--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
 CXX = g++
 CXXFLAGS = -std=c++26 -Iinclude -Isrc -Wall -Wextra -Wpedantic -Wshadow -Wold-style-cast -march=native -fsanitize=address -fsanitize=undefined 
-
 TEST_LDFLAGS = -lgtest -lgtest_main -pthread -lgmock
 
 TARGET = build/neogba
--- tests/arm7tdmi/isa/isa_constants_test.cpp
+++ tests/arm7tdmi/isa/isa_constants_test.cpp
@@ -3,17 +3,17 @@
 
 using namespace neogba;
 
-static constexpr u32 K_TEST_HALFIMM_INS{0xa17cdbd3u};
-static constexpr auto K_TEST_HALFIMM_INS_COND{0xa};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_P{true};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_U{false};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_W{true};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_L{true};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_RN{0xc};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_RD{0xd};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_S{true};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_H{false};
-static constexpr auto K_TEST_HALFIMM_INS_HALFIMM_OFFSET{0xb3};
+static constexpr u32 KTEST_INS{0xa17cdbd3u};
+static constexpr auto KTEST_COND{0xa};
+static constexpr auto KTEST_HALFIMM_P{true};
+static constexpr auto KTEST_HALFIMM_U{false};
+static constexpr auto KTEST_HALFIMM_W{true};
+static constexpr auto KTEST_HALFIMM_L{true};
+static constexpr auto KTEST_HALFIMM_RN{0xc};
+static constexpr auto KTEST_HALFIMM_RD{0xd};
+static constexpr auto KTEST_HALFIMM_S{true};
+static constexpr auto KTEST_HALFIMM_H{false};
+static constexpr auto KTEST_HALFIMM_OFFSET{0xb3};
 
 TEST(isa_test, should_change_only_arm_cond_field) {
   using namespace arm_cond;
@@ -62,18 +62,18 @@ TEST(isa_test, should_retrieve_right_bits_from_raw_arm_halfimm_instruction) {
   using namespace arm_halfimm;
   using namespace arm_cond;
 
-  const auto ins{K_TEST_HALFIMM_INS};
+  const auto ins{KTEST_INS};
 
-  ASSERT_EQ(K_TEST_HALFIMM_INS_COND, COND::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_P, P::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_U, U::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_W, W::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_L, L::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_RN, RN::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_RD, RD::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_S, S::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_H, H::get(ins));
-  ASSERT_EQ(K_TEST_HALFIMM_INS_HALFIMM_OFFSET, OFFSET::get(ins));
+  ASSERT_EQ(KTEST_COND, COND::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_P, P::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_U, U::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_W, W::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_L, L::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_RN, RN::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_RD, RD::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_S, S::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_H, H::get(ins));
+  ASSERT_EQ(KTEST_HALFIMM_OFFSET, OFFSET::get(ins));
 }
 
 TEST(isa_test, should_build_ri_bits_from_raw_arm_halfimm_instruction) {
@@ -82,18 +82,29 @@ TEST(isa_test, should_build_ri_bits_from_raw_arm_halfimm_instruction) {
 
   auto ins{TEMPLATE};
 
-  ins = COND::set(ins, K_TEST_HALFIMM_INS_COND);
+  ins = COND::set(ins, KTEST_COND);
   ins = P::set1(ins);
   ins = U::set0(ins);
   ins = W::toggle(ins);
-  ins = L::set(ins, K_TEST_HALFIMM_INS_HALFIMM_L);
-  ins = RN::set(ins, K_TEST_HALFIMM_INS_HALFIMM_RN);
-  ins = RD::set(ins, K_TEST_HALFIMM_INS_HALFIMM_RD);
-  ins = S::set(ins, K_TEST_HALFIMM_INS_HALFIMM_S);
+  ins = L::set(ins, KTEST_HALFIMM_L);
+  ins = RN::set(ins, KTEST_HALFIMM_RN);
+  ins = RD::set(ins, KTEST_HALFIMM_RD);
+  ins = S::set(ins, KTEST_HALFIMM_S);
   ins = H::set0(ins);
-  ins = OFFSET::set(ins, K_TEST_HALFIMM_INS_HALFIMM_OFFSET);
+  ins = OFFSET::set(ins, KTEST_HALFIMM_OFFSET);
 
-  ASSERT_EQ(K_TEST_HALFIMM_INS, ins);
+  ASSERT_EQ(KTEST_INS, ins);
+}
+
+TEST(isa_test, should_build_ri_bits_from_raw_arm_halfimm_instruction_with_newer_syntax) {
+  using namespace arm_halfimm;
+  using namespace arm_cond;
+
+  auto ins{TEMPLATE | COND::h(KTEST_COND) | P::H | U::h(false) | W::h(1) | L::h(KTEST_HALFIMM_L) |
+           RN::h(KTEST_HALFIMM_RN) | RD::h(KTEST_HALFIMM_RD) | S::h(KTEST_HALFIMM_S) |
+           OFFSET::h(KTEST_HALFIMM_OFFSET)};
+
+  ASSERT_EQ(KTEST_INS, ins);
 }
 
 TEST(isa_test, should_return_same_when_building_its_refered_to_the_same_range_of_bits) {
