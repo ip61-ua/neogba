930c77f49369d86ba1e70b8e668eb25ca6aaa194
ip61-ua <ip61@alu.ua.es>
2026-08-12 15:39:03 +0200

    test: reorganize test in folders!

--- tests/arm7tdmi/basic_fetch_decode_execute_test.cpp
+++ tests/arm7tdmi/basic_fetch_decode_execute_test.cpp
@@ -35,14 +35,14 @@ protected:
 } // namespace
 
 TEST_F(cpu_basic_lifecycle_test, fetches_decodes_and_executes_sequential_arm_instructions) {
-  constexpr u32 i0 = ISA_ARM_FSR_TEMPLATE | ISA_ARM_COND::set_high(arm_cond::AL) |
-                     ISA_ARM_FSR_OPCODE::set_high(arm_fsr_opcode::MOV) |
-                     ISA_ARM_FSR_RD::set_high(r1) | ISA_ARM_FSR_I::set_high() |
-                     ISA_ARM_FSR_OPERAND2_IMM::set_high(42u),
-                i1 = ISA_ARM_FSR_TEMPLATE | ISA_ARM_COND::set_high(arm_cond::AL) |
-                     ISA_ARM_FSR_OPCODE::set_high(arm_fsr_opcode::MOV) |
-                     ISA_ARM_FSR_RD::set_high(r2) | ISA_ARM_FSR_I::set_high() |
-                     ISA_ARM_FSR_OPERAND2_IMM::set_high(69u);
+  using namespace neogba::arm_operand2;
+  using namespace neogba::arm_fsr;
+  using namespace neogba::arm_cond;
+
+  constexpr u32 i0 = TEMPLATE | COND::set_high(cond_enum::AL) | OPCODE::set_high(opcode_enum::MOV) |
+                     RD::set_high(r1) | I::set_high() | IMM::set_high(42u),
+                i1 = TEMPLATE | COND::set_high(cond_enum::AL) | OPCODE::set_high(opcode_enum::MOV) |
+                     RD::set_high(r2) | I::set_high() | IMM::set_high(69u);
 
   ASSERT_TRUE(bus->write(32, RAM_BASE_ADDR, i0));
   ASSERT_TRUE(bus->write(32, RAM_BASE_ADDR + 4, i1));
