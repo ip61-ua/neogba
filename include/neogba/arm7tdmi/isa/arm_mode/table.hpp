e98b98791539cd50aff52a7be6b655da9d9ad710
ip61-ua <ip61@alu.ua.es>
2026-09-10 10:37:31 +0200

    feat: fill lut table with single data swap instruction type

--- include/neogba/arm7tdmi/isa/arm_mode/table.hpp
+++ include/neogba/arm7tdmi/isa/arm_mode/table.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "neogba/arm7tdmi/isa/arm_mode/blocktrans.hpp"
 #include "neogba/arm7tdmi/isa/arm_mode/fsr.hpp"
+#include "neogba/arm7tdmi/isa/arm_mode/singleswap.hpp"
 #include "neogba/arm7tdmi/isa/arm_mode/singletrans.hpp"
 #include "neogba/arm7tdmi/isa/arm_mode/undefined.hpp"
 #include "neogba/arm7tdmi/isa/constants.hpp"
@@ -18,59 +19,77 @@ inline constexpr auto arm_mode_lut = []() consteval {
 
   table.fill(arm_undefined::undefined);
 
-  [&]<std::size_t... Is>(std::index_sequence<Is...>) {
-    (([&]() {
-       using namespace arm_fsr;
-       constexpr auto opcode{static_cast<opcode_enum>(Is & 0xFu)};
-       constexpr bool s{((Is >> 4) & 1u) != 0};
+  {
+    [&]<std::size_t... Is>(std::index_sequence<Is...>) {
+      (([&]() {
+         using namespace arm_fsr;
+         constexpr auto opcode{static_cast<opcode_enum>(Is & 0xFu)};
+         constexpr bool s{((Is >> 4) & 1u) != 0};
 
-       if constexpr (!(not s and (opcode == opcode_enum::TST or opcode == opcode_enum::TEQ or
-                                  opcode == opcode_enum::CMP or opcode == opcode_enum::CMN))) {
+         if constexpr (!(not s and (opcode == opcode_enum::TST or opcode == opcode_enum::TEQ or
+                                    opcode == opcode_enum::CMP or opcode == opcode_enum::CMN))) {
 
-         table.fill(TEMPLATE | OPCODE::h(opcode) | S::h(s), IGNORED,
-                    &fsr<fsr_tflags{.opcode = opcode, .s = s}>);
-       }
-     }()),
-     ...);
-  }(std::make_index_sequence<32>{});
+           table.fill(TEMPLATE | OPCODE::h(opcode) | S::h(s), IGNORED,
+                      &fsr<fsr_tflags{.opcode = opcode, .s = s}>);
+         }
+       }()),
+       ...);
+    }(std::make_index_sequence<32>{});
+  }
 
-  [&]<std::size_t... Is>(std::index_sequence<Is...>) {
-    (([&]() {
-       using namespace arm_singletrans;
-       constexpr u32 tflags{0x3fu & Is};
-       constexpr auto i{(tflags & (0x1u << 5)) != 0}, //
-           p{(tflags & (0x1u << 4)) != 0},            //
-           u{(tflags & (0x1u << 3)) != 0},            //
-           b{(tflags & (0x1u << 2)) != 0},            //
-           w{(tflags & (0x1u << 1)) != 0},            //
-           l{(tflags & 0x1u) != 0};
+  {
+    [&]<std::size_t... Is>(std::index_sequence<Is...>) {
+      (([&]() {
+         using namespace arm_singletrans;
+         constexpr u32 tflags{0x3fu & Is};
+         constexpr auto i{(tflags & (0x1u << 5)) != 0}, //
+             p{(tflags & (0x1u << 4)) != 0},            //
+             u{(tflags & (0x1u << 3)) != 0},            //
+             b{(tflags & (0x1u << 2)) != 0},            //
+             w{(tflags & (0x1u << 1)) != 0},            //
+             l{(tflags & 0x1u) != 0};
 
-       table.fill(TEMPLATE | I::h(i) | P::h(p) | U::h(u) | B::h(b) | W::h(w) | L::h(l), IGNORED,
-                  &singletrans<singletrans_tflags{i, p, u, b, w, l}>);
-     }()),
-     ...);
-  }(std::make_index_sequence<64>{});
+         table.fill(TEMPLATE | I::h(i) | P::h(p) | U::h(u) | B::h(b) | W::h(w) | L::h(l), IGNORED,
+                    &singletrans<singletrans_tflags{i, p, u, b, w, l}>);
+       }()),
+       ...);
+    }(std::make_index_sequence<64>{});
+  }
 
-  [&]<std::size_t... Is>(std::index_sequence<Is...>) {
-    (([&]() {
-       // Primero se llena con bits[7..4] con any number
-       // Y luego con cero
-       using namespace arm_blocktrans;
-       constexpr u32 tflags{0x3fu & Is};
-       constexpr auto zeros_4_to_7{(tflags & (0x1u << 5)) != 0}, //
-           p{(tflags & (0x1u << 4)) != 0},                       //
-           u{(tflags & (0x1u << 3)) != 0},                       //
-           s{(tflags & (0x1u << 2)) != 0},                       //
-           w{(tflags & (0x1u << 1)) != 0},                       //
-           l{(tflags & 0x1u) != 0};                              //
+  {
+    [&]<std::size_t... Is>(std::index_sequence<Is...>) {
+      (([&]() {
+         // Primero se llena con bits[7..4] con any number
+         // Y luego con cero
+         using namespace arm_blocktrans;
+         constexpr u32 tflags{0x3fu & Is};
+         constexpr auto zeros_4_to_7{(tflags & (0x1u << 5)) != 0}, //
+             p{(tflags & (0x1u << 4)) != 0},                       //
+             u{(tflags & (0x1u << 3)) != 0},                       //
+             s{(tflags & (0x1u << 2)) != 0},                       //
+             w{(tflags & (0x1u << 1)) != 0},                       //
+             l{(tflags & 0x1u) != 0};                              //
 
-       table.fill(TEMPLATE | P::h(p) | U::h(u) | S::h(s) | W::h(w) | L::h(l),
-                  IGNORED | (zeros_4_to_7 ? 0 : REGLIST_7_4::H),
-                  &blocktrans<blocktrans_tflags{p, u, s, w, l, zeros_4_to_7}>);
-     }()),
-     ...);
-  }(std::make_index_sequence<64>{});
+         table.fill(TEMPLATE | P::h(p) | U::h(u) | S::h(s) | W::h(w) | L::h(l),
+                    IGNORED | (zeros_4_to_7 ? 0 : REGLIST_7_4::H),
+                    &blocktrans<blocktrans_tflags{p, u, s, w, l, zeros_4_to_7}>);
+       }()),
+       ...);
+    }(std::make_index_sequence<64>{});
+  }
 
+  {
+    [&]<std::size_t... Is>(std::index_sequence<Is...>) {
+      (([&]() {
+         using namespace arm_singleswap;
+
+         constexpr bool b{Is == 1};
+
+         table.fill(TEMPLATE | B::h(b), IGNORED, &singleswap<singleswap_tflags{b}>);
+       }()),
+       ...);
+    }(std::make_index_sequence<2>{});
+  }
   return table;
 }();
 } // namespace neogba
